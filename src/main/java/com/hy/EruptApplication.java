f07c84918ec44a015075aea6ca4723f46664acdd
HY-love-sleep <583699747@qq.com>
2023-10-18 16:42:12 +0800

    init commit

---
+++ src/main/java/com/hy/EruptApplication.java
@@ -0,0 +1,16 @@
+package com.hy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description: erupt启动类
+ * Author: yhong
+ * Date: 2023/10/18
+ */
+@SpringBootApplication
+public class EruptApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EruptApplication.class, args);
+    }
+}
